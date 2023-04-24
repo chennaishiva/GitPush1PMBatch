1914f073772e95736c952ed431092d51a4af51ea
chennaishiva <chennaisiva21@gmail.com>
2023-04-24 13:45:48 +0530

    Just project push

---
+++ src/org/test/Sample.java
@@ -0,0 +1,19 @@
+package org.test;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(13);
+		System.out.println(14);
+		System.out.println(15);
+		System.out.println(176);
+		System.out.println(17);
+		System.out.println(18);
+		System.out.println(19);
+		System.out.println(112);
+	}
+
+}
