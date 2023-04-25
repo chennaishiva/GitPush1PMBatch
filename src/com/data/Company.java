bf5e3419c4fd639f853bb19098f9f28e0f58300d
chennaishiva <chennaisiva21@gmail.com>
2023-04-25 13:27:14 +0530

    A works

---
+++ src/com/data/Company.java
@@ -0,0 +1,15 @@
+package com.data;
+
+public class Company {
+	
+	private void add() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void sub() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
